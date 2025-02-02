ee84708bc202612a3948d8fe0accf3bfe92ee5c4
xxoo6699 <xuyzo@icloud.com>
2025-02-03 02:52:03 +0800

    Update

--- apps/remix/app/routes/_h.terms.tsx
+++ apps/remix/app/routes/_h.terms.tsx
@@ -19,7 +19,7 @@ export default function Index() {
           Terms of Service
         </h1>
         <p className="mt-8">
-          Welcome to Vmail.DEV! These Terms of Service ("Terms") govern your
+          Welcome to MAIL.NONULL! These Terms of Service ("Terms") govern your
           access to and use of our website and services. By accessing or using
           our services, you agree to be bound by these Terms. If you do not
           agree to these Terms, please do not use our services.
@@ -29,12 +29,12 @@ export default function Index() {
           1. Description of Service
         </h2>
         <p className="mt-4">
-          Vmail.DEV is a minimalistic temporary email service that allows you to
+          MAIL.NONULL is a minimalistic temporary email service that allows you to
           obtain a temporary email address without disclosing any personal
           information, ensuring the protection of your privacy. Our service
           supports the selection of multiple domain names, providing you with
           the convenience of using it in various scenarios. Additionally,
-          Vmail.DEV operates on the Cloudflare network, guaranteeing a
+          MAIL.NONULL operates on the Cloudflare network, guaranteeing a
           super-fast experience.
         </p>
 
@@ -42,14 +42,8 @@ export default function Index() {
           2. Usage Restrictions
         </h2>
         <p className="mt-4">
-          2.1 Availability: Please note that Vmail.DEV is not available in China
+          2.1 Availability: Please note that MAIL.NONULL is not available in China
           Mainland. We apologize for any inconvenience caused.
-        </p>
-        <p className="mt-4">
-          2.2 Copyright: All copyrights related to Vmail.DEV and its services
-          belong to Vmail.DEV. You may not copy, reproduce, distribute, modify,
-          or create derivative works of Vmail.DEV or any part thereof without
-          explicit permission from Vmail.DEV.
         </p>
 
         <h2 className="mt-8 text-2xl font-bold" id="Deletion of Emails">
@@ -57,12 +51,12 @@ export default function Index() {
         </h2>
         <p className="mt-4">
           3.1 Registration: You are not required to register or provide any
-          personal information to use Vmail.DEV. However, you are solely
+          personal information to use MAIL.NONULL. However, you are solely
           responsible for maintaining the confidentiality of any temporary email
           addresses generated through our service.
         </p>
         <p className="mt-4">
-          3.2 Prohibited Activities: While using Vmail.DEV, you agree not to
+          3.2 Prohibited Activities: While using MAIL.NONULL, you agree not to
           engage in any activities that may violate any applicable laws,
           regulations, or these Terms. Prohibited activities include, but are
           not limited to:
@@ -82,9 +76,9 @@ export default function Index() {
           4. Disclaimer of Warranty
         </h2>
         <p className="mt-4">
-          Vmail.DEV is provided on an "as is" and "as available" basis. We do
+          MAIL.NONULL is provided on an "as is" and "as available" basis. We do
           not warrant that our services will be uninterrupted, error-free, or
-          secure. Your use of Vmail.DEV is at your own risk, and we disclaim all
+          secure. Your use of MAIL.NONULL is at your own risk, and we disclaim all
           warranties, whether express or implied, including but not limited to
           warranties of merchantability, fitness for a particular purpose, and
           non-infringement.
@@ -94,12 +88,12 @@ export default function Index() {
           5. Limitation of Liability
         </h2>
         <p className="mt-4">
-          To the maximum extent permitted by law, in no event shall Vmail.DEV or
+          To the maximum extent permitted by law, in no event shall MAIL.NONULL or
           its affiliates be liable for any indirect, incidental, special,
           consequential, or punitive damages, including without limitation, loss
           of profits, data, use, goodwill, or other intangible losses, resulting
-          from your access to or use of Vmail.DEV or any content or services
-          provided through Vmail.DEV.
+          from your access to or use of MAIL.NONULL or any content or services
+          provided through MAIL.NONULL.
         </p>
 
         <h2 className="mt-8 text-2xl font-bold" id="">
@@ -108,7 +102,7 @@ export default function Index() {
         <p className="mt-4">
           We reserve the right to modify these Terms at any time, without prior
           notice. Any changes to the Terms will be effective immediately upon
-          posting. Your continued use of Vmail.DEV after the posting of any
+          posting. Your continued use of MAIL.NONULL after the posting of any
           modified Terms constitutes your acceptance of such changes.
         </p>
 
@@ -117,7 +111,7 @@ export default function Index() {
         </h2>
         <p className="mt-4">
           These Terms shall be governed by and construed in accordance with the
-          laws of the jurisdiction where Vmail.DEV is located. Any disputes
+          laws of the jurisdiction where MAIL.NONULL is located. Any disputes
           arising out of or in connection with these Terms shall be subject to
           the exclusive jurisdiction of the courts in that jurisdiction.
         </p>
@@ -126,7 +120,7 @@ export default function Index() {
           If you have any questions or concerns regarding these Terms, please
           contact us.
         </p>
-        <p className="mt-8">Last updated: 2024-04-10</p>
+        <p className="mt-8">Last updated: 1999-01-01</p>
       </div>
     </div>
   );
